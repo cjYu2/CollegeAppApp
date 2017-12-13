af4d9f72c62b1732e1897a3722e4aea042e4c216
CJ Yu <cjy7280x@gmail.com>
2017-12-13 08:29:43 -0800

    First commit

---
+++ app/src/main/java/com/example/cj/collegeappapp/HomeActivity.java
@@ -0,0 +1,31 @@
+package com.example.cj.collegeappapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, LoginActivity.class);
+    }
+
+}
